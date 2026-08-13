ae9207bf4cc472d0a830a04df874122ed43ec40a
QA Explorer <tracyojoseph@yahoo.com>
2026-08-13 19:04:36 -0400

    Refactor goToLogin method to use By locator
    Updated the goToLogin method to use a locator for the login link and increased the wait duration.

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,12 +1,11 @@
 package pageObjects;
-
-
 import java.time.Duration;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
 
@@ -25,10 +24,13 @@ public class HomePage extends BasePage {
         wait.until(ExpectedConditions.elementToBeClickable(link_MyAccount));
         link_MyAccount.click();
     }
-
     public void goToLogin() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(link_login));
-        link_login.click();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    By loginLocator = By.xpath("//a[normalize-space()='Login']");
+    WebElement loginLink = wait.until(
+    ExpectedConditions.elementToBeClickable(loginLocator)
+    );
+    loginLink.click();
+  
     }
 }
