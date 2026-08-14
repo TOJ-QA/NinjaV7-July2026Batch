b82667f44a3b67542bb730246f4b583089f49aaa
QA Explorer <tracyojoseph@yahoo.com>
2026-08-13 21:37:36 -0400

    Refactor HomePage methods for better element interaction
    Refactor clickMyAccount and goToLogin methods to improve element handling and scrolling.

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,11 +1,14 @@
 package pageObjects;
+
 import java.time.Duration;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage {
 
@@ -13,24 +16,58 @@ public class HomePage extends BasePage {
         super(driver);
     }
 
-    @FindBy(xpath="//i[@class='fa-solid fa-user']")
+    @FindBy(xpath = "//i[@class='fa-solid fa-user']")
     WebElement link_MyAccount;
 
-    @FindBy(xpath="//a[normalize-space()='Login']")
+    @FindBy(xpath = "//a[normalize-space()='Login']")
     WebElement link_login;
 
     public void clickMyAccount() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(link_MyAccount));
-        link_MyAccount.click();
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        WebElement myAccount = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        link_MyAccount
+                )
+        );
+
+        myAccount.click();
     }
+
     public void goToLogin() {
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-    By loginLocator = By.xpath("//a[normalize-space()='Login']");
-    WebElement loginLink = wait.until(
-    ExpectedConditions.elementToBeClickable(loginLocator)
-    );
-    loginLink.click();
-  
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        By loginLocator =
+                By.xpath("//a[normalize-space()='Login']");
+
+        WebElement loginLink = wait.until(
+                ExpectedConditions.presenceOfElementLocated(
+                        loginLocator
+                )
+        );
+
+        wait.until(
+                ExpectedConditions.visibilityOf(loginLink)
+        );
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView({block:'center', inline:'center'});",
+                loginLink
+        );
+
+        loginLink = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        loginLocator
+                )
+        );
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].click();",
+                loginLink
+        );
     }
 }
