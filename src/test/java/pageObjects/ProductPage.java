148bce334ee046cb744b65a15f867e3947535d18
QA Explorer <tracyojoseph@yahoo.com>
2026-08-13 20:15:05 -0400

    Refactor ProductPage class for improved element handling

--- src/test/java/pageObjects/ProductPage.java
+++ src/test/java/pageObjects/ProductPage.java
@@ -1,81 +1,156 @@
 package pageObjects;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-//import org.openqa.selenium.By;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ProductPage extends BasePage
-{
-	    public ProductPage(WebDriver driver) 
-	    {
-	        super(driver);
-	    }
-	    @FindBy(xpath = "//input[@id='input-option-225']")
-	    WebElement input_DeliveryDate;
+public class ProductPage extends BasePage {
 
-	    @FindBy(xpath = "//button[@id='button-cart']")
-	    WebElement btn_AddToCart;
+    public ProductPage(WebDriver driver) {
+        super(driver);
+    }
 
-	    @FindBy(xpath = "//div[@class='alert alert-success alert-dismissible']")
-	    WebElement alert_Success;
-	    
-	    @FindBy(xpath = "//a[@title='Checkout']//i[@class='fa-solid fa-share']")
-	    WebElement link_Checkout;
-	    	    
-	    @FindBy(xpath = "//div//button//i[@class='fa-solid fa-heart']")
-	    WebElement wishlistIcon;
+    @FindBy(xpath = "//input[@id='input-option-225']")
+    WebElement input_DeliveryDate;
 
-	    @FindBy(xpath = "//div[@class='alert alert-success alert-dismissible']")
-	    WebElement successMessage;
+    @FindBy(xpath = "//button[@id='button-cart']")
+    WebElement btn_AddToCart;
 
-	    public void addToWishlist() 
-	    {
-	       	// ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", wishlistIcon);
-	        wishlistIcon.click();
-	    }
-	    public void setDeliveryDate() {
-	        LocalDate date = LocalDate.now().plusDays(5);
-	        String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    @FindBy(xpath = "//div[@class='alert alert-success alert-dismissible']")
+    WebElement alert_Success;
 
-	        scrollToView(input_DeliveryDate);
-	        input_DeliveryDate.clear();
-	        input_DeliveryDate.sendKeys(formattedDate);
-	    }
-	    
-	    public void clickAddToCart() throws InterruptedException {
-	    	// Scroll into view
-			((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", btn_AddToCart);
+    @FindBy(xpath = "//a[@title='Checkout']//i[@class='fa-solid fa-share']")
+    WebElement link_Checkout;
 
-			// Optional: Add a small wait after scrolling to let it stabilize
-			Thread.sleep(500);
+    @FindBy(xpath = "//div//button//i[@class='fa-solid fa-heart']")
+    WebElement wishlistIcon;
 
-	        btn_AddToCart.click();
-	    }
+    @FindBy(xpath = "//div[@class='alert alert-success alert-dismissible']")
+    WebElement successMessage;
 
-	    public boolean isSuccessMessageDisplayed() {
-	        return alert_Success.getText().contains("Success");
-	    }
+    public void addToWishlist() {
 
-	    private void scrollToView(WebElement element) {
-	        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-	    }
-	    
-	    public void clickCheckout() throws InterruptedException 
-		{
-					
-	    	        Thread.sleep(500);
-					// Scroll into view
-					((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", link_Checkout);
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
 
-					// Optional: Add a small wait after scrolling to let it stabilize
-					Thread.sleep(500);
+        By wishlistLocator =
+                By.xpath("//div//button//i[@class='fa-solid fa-heart']");
 
-					// Now clear
-					link_Checkout.click();
-		}
-	    
-	}
+        WebElement wishlist = wait.until(
+                ExpectedConditions.presenceOfElementLocated(wishlistLocator)
+        );
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView({block:'center'});",
+                wishlist
+        );
+
+        wishlist = wait.until(
+                ExpectedConditions.elementToBeClickable(wishlistLocator)
+        );
+
+        try {
+            wishlist.click();
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+
+            WebElement button = wishlist.findElement(By.xpath("./.."));
+
+            ((JavascriptExecutor) driver).executeScript(
+                    "arguments[0].click();",
+                    button
+            );
+        }
+    }
+
+    public void setDeliveryDate() {
+
+        LocalDate date = LocalDate.now().plusDays(5);
+
+        String formattedDate =
+                date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        scrollToView(input_DeliveryDate);
+
+        input_DeliveryDate.clear();
+        input_DeliveryDate.sendKeys(formattedDate);
+    }
+
+    public void clickAddToCart() throws InterruptedException {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        By cartLocator = By.id("button-cart");
+
+        WebElement cartButton = wait.until(
+                ExpectedConditions.elementToBeClickable(cartLocator)
+        );
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView({block:'center'});",
+                cartButton
+        );
+
+        cartButton = wait.until(
+                ExpectedConditions.elementToBeClickable(cartLocator)
+        );
+
+        cartButton.click();
+    }
+
+    public boolean isSuccessMessageDisplayed() {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        WebElement success = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath(
+                                "//div[@class='alert alert-success alert-dismissible']"
+                        )
+                )
+        );
+
+        return success.getText().contains("Success");
+    }
+
+    public void clickCheckout() throws InterruptedException {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        By checkoutLocator =
+                By.xpath("//a[@title='Checkout']//i[@class='fa-solid fa-share']");
+
+        WebElement checkout = wait.until(
+                ExpectedConditions.presenceOfElementLocated(checkoutLocator)
+        );
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView({block:'center'});",
+                checkout
+        );
+
+        checkout = wait.until(
+                ExpectedConditions.elementToBeClickable(checkoutLocator)
+        );
+
+        checkout.click();
+    }
+
+    private void scrollToView(WebElement element) {
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView({block:'center'});",
+                element
+        );
+    }
+}
